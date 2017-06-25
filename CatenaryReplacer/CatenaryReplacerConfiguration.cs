72d2579f2ccb35fb123610eb9466bb0ad3d8d410
Egor Aralov <aei.omsk@gmail.com>
2017-06-25 23:35:04 +0200

    Hide new option for time being

--- CatenaryReplacer/CatenaryReplacerConfiguration.cs
+++ CatenaryReplacer/CatenaryReplacerConfiguration.cs
@@ -5,10 +5,19 @@ namespace CatenaryReplacer
     [Options("CatenaryReplacer.xml", "CatenaryReplacer.xml")]
     public class CatenaryReplacerConfiguration
     {
+        [AlwaysHide]
         [Checkbox("Replace catenaries", null, nameof(ReplacementHandler), nameof(ReplacementHandler.ReplaceBool))]
         public bool ReplacementEnabled { get; set; } = true;
 
         [DropDown("Catenary Style", nameof(CatenaryStyle), null, nameof(ReplacementHandler), nameof(ReplacementHandler.ReplaceInt))]
         public int Style { get; set; } = (int) CatenaryStyle.German;
+
+        public class AlwaysHideAttribute : HideConditionAttribute
+        {
+            public override bool IsHidden()
+            {
+                return true;
+            }
+        }
     }
 }
